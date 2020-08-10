ffdb99c090767dca5953fb64906cd782943d6101
Jared Lunde <jared.lunde@gmail.com>
2020-08-10 14:33:57 -0600

    chore(release): 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [1.0.2](https://github.com/jaredLunde/forward-ref-as/compare/v1.0.1...v1.0.2) (2020-08-10)
+
+### Bug Fixes
+
+- **types:** fix prop extraction for extrinsic components ([4cc7c74](https://github.com/jaredLunde/forward-ref-as/commit/4cc7c74c0984039425231e3224686e655b14df07))
+
 ### [1.0.1](https://github.com/jaredLunde/forward-ref-as/compare/v1.0.0...v1.0.1) (2020-07-25)
 
 ## 1.0.0 (2020-07-25)
--- types/index.d.ts
+++ types/index.d.ts
@@ -7,12 +7,7 @@ import * as React from 'react'
  * forwardRefAs<ButtonProps, 'button'>(ButtonComponent)
  */
 declare function forwardRefAs<Props, DefaultAs extends AsProp = 'div'>(
-  render: React.RefForwardingComponent<
-    DefaultAs extends keyof JSX.IntrinsicElements
-      ? FromReactType<DefaultAs>
-      : DefaultAs,
-    Props
-  >
+  render: React.RefForwardingComponent<PropsOf<DefaultAs>, Props>
 ): ForwardRefAsExoticComponent<Props, DefaultAs>
 export default forwardRefAs
 /**
@@ -34,9 +29,7 @@ export declare type ForwardRefAsExoticComponent<
       } & Props,
       React.ComponentProps<As>
     > &
-      React.RefAttributes<
-        As extends keyof JSX.IntrinsicElements ? FromReactType<As> : As
-      >
+      React.RefAttributes<PropsOf<As>>
   ): JSX.Element | null
   defaultProps: {
     as?: AsProp
@@ -45,30 +38,16 @@ export declare type ForwardRefAsExoticComponent<
   displayName: string
 }
 /**
- * Maps a keyof JSX.IntrinsicElement (e.g. 'div' or 'svg') or a
- * React.ComponentType to it's type.
- *
- * For example:
- *   FromReactType<"div"> ==> HTMLDivElement
- *   FromReactType<"svg"> ==> SVGSVGElement
- *   FromReactType<React.FC<P>. ==> React.FC<P>
- */
-export declare type FromReactType<
-  T extends React.ReactType
-> = T extends keyof JSX.IntrinsicElements
-  ? JSX.IntrinsicElements[T] extends React.DetailedHTMLFactory<
-      React.HTMLAttributes<infer U>,
-      infer U
-    >
-    ? U
-    : JSX.IntrinsicElements[T] extends React.SVGProps<infer V>
-    ? V
-    : never
-  : T
-/**
  * Omits an props in `T` that are already present in `P`
  */
 export declare type Prefer<P, T> = P & Omit<T, keyof P>
+/**
+ * Extracts the props of a component type
+ * Credit: Emotion
+ */
+export declare type PropsOf<
+  E extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
+> = JSX.LibraryManagedAttributes<E, React.ComponentPropsWithRef<E>>
 /**
  * These are the types accepted by the "as" prop in layout components
  */
